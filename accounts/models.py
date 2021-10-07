20f60820d1fb3cd9b64393baa0663d58d6d2a591
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2021-10-07 20:01:43 +0530

    Update models.py

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+
+
+class petOwner(models.Model):
+    yourname = models.CharField(max_length=255, default='')
+    ownerImage = models.ImageField(
+        upload_to='Images/', default='Images/user_profile1.png', null=True)
+    contact = models.CharField(max_length=13, default='')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    username = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.username
+
+
+class petDoctor(models.Model):
+    yourname = models.CharField(max_length=255, default='')
+    doctorImage = models.ImageField(
+        upload_to='Images/', default='Images/user_profile1.png', null=True)
+    contact = models.CharField(max_length=13, default='')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, default='')
+    username = models.CharField(max_length=255, default='')
+
+    def __str__(self):
+        return self.username
