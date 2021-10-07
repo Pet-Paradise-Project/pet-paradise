dfbef6d4ecc2669e94388b4694e720b09d1de400
Yash Mistry <76916876+yashms25@users.noreply.github.com>
2021-10-07 21:01:26 +0530

    Update admin.py

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,21 @@
+
 from django.contrib import admin
 
 # Register your models here.
+from .models import petOwner, petDoctor
+
+
+class OwnerAdmin(admin.ModelAdmin):
+    list_display = ('username', 'yourname', 'email',
+                    'contact', 'ownerImage')
+
+
+admin.site.register(petOwner, OwnerAdmin)
+
+
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('username','yourname', 'email',
+                    'contact', 'doctorImage')
+
+
+admin.site.register(petDoctor, DoctorAdmin)
